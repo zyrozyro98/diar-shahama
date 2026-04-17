ffa8456a23d1653b43fddad6528da8eef8b50915
unknown <zyrozyro98@gmail.com>
2026-04-17 03:21:47 +0300

    Fix CORS xhr poll error for Codespaces

--- whatsapp-server/server.js
+++ whatsapp-server/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"], credentials: true },
+    cors: { origin: "*", methods: ["GET", "POST"] },
     allowEIO3: true
 });
 
