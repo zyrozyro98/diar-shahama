79cbbd25a3fae9cc555c737eee50a01aec539cd8
zyrozyro98 <zyrozyro98@gmail.com>
2026-04-20 02:11:16 +0300

    Update deploy_site.bat

--- deploy_site.bat
+++ deploy_site.bat
@@ -26,9 +26,6 @@ if %ERRORLEVEL% NEQ 0 (
 echo.
 echo [3/3] Uploading to Free Hosting (Surge.sh)...
 echo.
-echo   >> سيطلب منك النظام الآن إدخال بياناتك:
-echo   >> البريد: zyrozyro98@gmail.com
-echo   >> كلمة المرور: 770088254
 echo.
 
 :: Try local surge first, then npx
